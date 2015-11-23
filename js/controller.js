979ba081a7fa21c76b6f46e3262d11ccd6df11c9
oliverlai55 <oliverlai55@gmail.com>
2015-11-23 13:41:24 -0500

    js changes

--- js/controller.js
+++ js/controller.js
@@ -19,9 +19,13 @@ deltaSocial.controller('deltaController', function ($scope, $http, $location, $i
 
 	var url = "http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/delta/instagram/";
 
-	$http.get(url).success(function(data){
-		console.log(data);
-		var instagramData = data.data.images.low_resolution.url;
+	$http.get(url).success(function(instaData){
+		console.log(instaData);
+		var instagramData = [];
+		for(i = 0; i < 10; i++){
+			instagramData.push(instaData.data[i].images.standard_resolution.url);
+		}
+
 		$scope.imageUrl = instagramData;
 
 	})
