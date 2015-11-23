28b3dffa3e6960df5969f80f4ec4d2ea906a8aad
Griffin Hammer <grifhammer@gmail.com>
2015-11-23 14:37:29 -0500

    Removed second app declaration to fix conflict and changed the Facebook controller to deltaSocial

--- js/facebookController.js
+++ js/facebookController.js
@@ -1,5 +1,4 @@
-var deltaApp = angular.module('deltaSocial',[])
-deltaApp.controller('facebookCtrl', function ($scope, $http){
+deltaSocial.controller('facebookCtrl', function ($scope, $http){
 
 	var facebookUrl = "http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/delta/facebook/"
 	$http.get(facebookUrl).success(function (deltaData){
