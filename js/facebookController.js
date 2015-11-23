834daf57121415ef95482764ff77b32345812810
Frederick Calk <Freddy@Fredericks-Air.arrayfire.net>
2015-11-23 14:38:31 -0500

    Committing the changes made in the ATL branch

--- js/facebookController.js
+++ js/facebookController.js
@@ -1,9 +1,12 @@
-var deltaApp = angular.module('deltaSocial',[])
-deltaApp.controller('facebookCtrl', function ($scope, $http){
+var deltaSocial = angular.module('deltaSocial',[])
+deltaSocial.controller('facebookCtrl', function ($scope, $http){
 
 	var facebookUrl = "http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/delta/facebook/"
+
 	$http.get(facebookUrl).success(function (deltaData){
-		console.log(deltaData.data);
+
 		$scope.posts = deltaData.data;
+
 	})
+
 })
