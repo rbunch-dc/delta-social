ad69c32601bf95675bcd02a0480e60d792159b95
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-11-23 09:59:36 -0500

    Init repo and boilerplate

---
+++ js/controller.js
@@ -0,0 +1,20 @@
+var deltaSocial = angular.module('deltaSocial', ['ngRoute']);
+
+// deltaApp.config(function($routeProvider, $locationProvider){
+// 	$routeProvider.when('/', {
+// 		templateUrl: 'XXXXX',
+// 		controller: 'deltaController'
+// 	}).
+// 	when('/XXXX',{
+// 		templateUrl: '',
+// 		controller: ''
+// 	}).
+// 	otherwise({
+// 		redirectTo: ''    // --default page to display.
+// 	});
+
+// });
+
+deltaSocial.controller('deltaController', function ($scope, $http, $location, $interval){
+
+});
