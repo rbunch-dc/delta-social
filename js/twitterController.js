8bb05a51adfc2a01a1944037e73541bbdc2b2269
wkwyatt <wkwyatt1@gmail.com>
2015-11-24 14:02:38 -0500

    youtube styling

--- js/twitterController.js
+++ js/twitterController.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-  $('.jcarousel').jcarousel({
-  	
-  });
+  var jcarousel = $('.jcarousel');
+
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function () {
+                var carousel = $(this),
+                    width = carousel.innerWidth();
+
+                if (width >= 600) {
+                    width = width / 3;
+                } else if (width >= 350) {
+                    width = width / 2;
+                }
+
+                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
 });
 
 deltaSocial.controller('twitterController', function($scope, $http){
